33cd079e7bfafe2a4b3ace3666298c914d4e581d
Ameera <ameerathiwanka@gmail.com>
2025-04-02 12:46:58 +0530

    Initial Commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import { DefaultLayout } from "../layouts/Default";
+
+export const HomePage = () => {
+    return (
+        <DefaultLayout>
+        <h1>Welcome to the Home Page!</h1>
+        <p>This is the content of the home page.</p>
+        </DefaultLayout>
+        );
+    }
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,9 @@
+export const LandingPage = () => {
+    return (
+        <div>
+        <h1>Landing Page</h1>
+        <p>Welcome to the Landing Page</p>
+        </div>
+    );
+    }
+    
